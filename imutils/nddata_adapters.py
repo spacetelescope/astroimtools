f75a64f9da76dea0973177db07ae83d4cea2c1e0
Larry Bradley <larry.bradley@gmail.com>
2015-03-18 15:11:51 -0400

    Add basic_nddata_to_fits function

--- imutils/nddata_adapters.py
+++ imutils/nddata_adapters.py
@@ -4,11 +4,12 @@ Tools reading FITS files into NDData objects.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+import numpy as np
 import astropy.io.fits as fits
 from astropy.nddata import NDData
 
 
-__all__ = ['basic_fits_to_nddata']
+__all__ = ['basic_fits_to_nddata', 'basic_nddata_to_fits']
 
 
 def basic_fits_to_nddata(filename, exten=0):
@@ -38,3 +39,35 @@ def basic_fits_to_nddata(filename, exten=0):
     header = hdulist[exten].header
     data = hdulist[exten].data
     return NDData(data, meta=header)
+
+
+def basic_nddata_to_fits(nddata, filename, clobber=False):
+    """
+    Write a `~astropy.nddata.NDData` object to a FITS file.
+    """
+
+    hdu = fits.ImageHDU(data=nddata.data, header=nddata.meta)
+    hdu.header['EXTNAME'] = 'SCI'
+    hdus = [hdu]
+
+    if nddata.uncertainty is not None:
+        hdu = fits.ImageHDU(data=nddata.uncertainty.value)
+        hdu.header['EXTNAME'] = 'ERR'
+        hdus.append(hdu)
+
+    if nddata.dq is not None:
+        if nddata.mask is not None:
+            nddata.dq |= nddata.mask.astype(np.int)
+        hdu = fits.ImageHDU(data=nddata.dq)
+        hdu.header['EXTNAME'] = 'DQ'
+        hdus.append(hdu)
+    else:
+        if nddata.mask is not None:
+            hdu = fits.ImageHDU(data=nddata.mask.astype(np.int))
+            hdu.header['EXTNAME'] = 'DQ'
+            hdus.append(hdu)
+
+    hdulist = fits.HDUList(hdus)
+    hdulist.writeto(filename, clobber=clobber)
+
+    return
