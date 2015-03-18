a0fcd08534df7c1a1bf4b0deae446d35a4ac564c
Larry Bradley <larry.bradley@gmail.com>
2015-03-17 23:11:57 -0400

    Add block_reduce function

--- imutils/core.py
+++ imutils/core.py
@@ -8,9 +8,10 @@ import numpy as np
 from astropy.nddata import NDData
 from astropy import log
 import copy
+from astropy.wcs import WCS
 
 
-__all__ = ['StdUncertainty', 'imarith']
+__all__ = ['StdUncertainty', 'imarith', 'block_reduce']
 
 
 class StdUncertainty(object):
@@ -97,3 +98,79 @@ def imarith(nddata1, nddata2, operator, fill_value=0.0, keywords=None):
 
     return NDData(np.ma.filled(mdata, fill_value=fill_value),
                   uncertainty=uncertainty_out, mask=mdata.mask, meta=meta_out)
+
+
+def block_reduce(data, block_size, func=np.sum, wcs=None, wcs_origin=0):
+    """
+    Downsample data by applying a function to local blocks.
+    """
+
+    from skimage.measure import block_reduce
+    data = np.asanyarray(data)
+    if len(block_size) != data.ndim:
+        raise ValueError('`block_size` must have the same length as '
+                         '`data.shape`')
+
+    block_size = np.array([int(i) for i in block_size])
+    size_new = np.array(data.shape) // block_size
+    size_init = size_new * block_size
+    if size_init[0] != data.shape[0] or size_init[1] != data.shape[1]:
+        data = data[:size_init[0], :size_init[1]]   # trim data if necessary
+
+    data_reduced = block_reduce(data, tuple(block_size), func=func)
+
+    if wcs is not None:
+        if not isinstance(wcs, WCS):
+            raise ValueError('wcs must be an astropy.wcs.WCS object')
+        wcs_out = _scale_image_wcs(wcs, 1.0 / block_size, origin=wcs_origin)
+    else:
+        wcs_out = None
+
+    return data_reduced, wcs_out
+
+
+def _scale_image_wcs(wcs, scale, origin=0):
+    """
+    Scale the WCS for a 2D image.
+    """
+
+    # interally use scale in (x, y) order to match WCS order convention
+    scale = scale[::-1]
+
+    wcs_out = wcs.deepcopy()
+    wcs_out._naxis1 = int(wcs._naxis1 * scale[0])
+    wcs_out._naxis2 = int(wcs._naxis2 * scale[1])
+    origin = int(origin)
+    if origin == 0:
+        crpix_new = ((np.array(wcs.wcs.crpix) + 0.5) * scale) - 0.5
+    elif origin == 1:
+        crpix_new = ((np.array(wcs.wcs.crpix) - 0.5) * scale) + 0.5
+    else:
+        raise ValueError('origin must be 0 or 1')
+    wcs_out.wcs.crpix = tuple(crpix_new)
+
+    if not wcs.wcs.has_cd():
+        wcs_out.wcs.cdelt = tuple(np.array(wcs.wcs.cdelt) / scale)
+    else:
+        wcs_out.wcs.cd /= scale
+        # TODO: if aspect ratio changes, need to update PC matrix and remove
+        # CROTA1 and CROTA2?
+
+    # TODO: update SIP coefficients
+    # wcs_out = _scale_sip(wcs_out, block_ratio)
+
+    return wcs_out
+
+
+def _scale_sip(wcs, scale, origin=0):
+    """
+    Update SIP coefficients for an image scale change.
+    """
+
+    wcs_out = wcs.deepcopy()
+    # TODO: need to update SIP A and B matrices
+    # a = wcs.sip.a
+    # b = wcs.sip.b
+    # coef *= np.power(block_ratio[i], np.arange(len(coef)))
+
+    return wcs_out
