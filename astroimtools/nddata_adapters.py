7f1cefd96fde382165a298c0ebb1752bf44949aa
Larry Bradley <larry.bradley@gmail.com>
2015-12-11 13:03:18 -0500

    Update nddata_adapters

--- astroimtools/nddata_adapters.py
+++ astroimtools/nddata_adapters.py
@@ -20,13 +20,17 @@ def basic_fits_to_nddata(filename, exten=0):
     This is an *extremely* simple reader that reads data from only a
     single FITS extension.
 
+    Note the the primary FITS header will always be included in the
+    NDData meta `dict`, regardless of the value of ``exten``.
+
     Parameters
     ----------
     filename : str
         The path to a FITS file.
 
     exten : int, optional
-        The FITS extension number for the ``data`` array.  Default is 0.
+        The FITS extension number for array to place in the NDData
+        object.  The default is 0.
 
     Returns
     -------
@@ -36,21 +40,41 @@ def basic_fits_to_nddata(filename, exten=0):
         containing the FITS header as a python `dict`.
     """
 
-    hdulist = fits.open(filename)
-    header = hdulist[exten].header
-    data = hdulist[exten].data
+    with fits.open(filename) as hdulist:
+        header = hdulist[0].header
+        header += hdulist[exten].header
+        data = hdulist[exten].data
     return NDData(data, meta=header)
 
 
 def basic_nddata_to_fits(nddata, filename, clobber=False):
     """
     Write a `~astropy.nddata.NDData` object to a FITS file.
+
+    That NDData data value will be saved in an extension called 'SCI'.
+    This simple writer will also attempt to save NDData uncertainty and
+    mask to an 'ERROR' and 'MASK' extension, respectively.
+
+    If present, the NDData meta dictionary will be stored as the FITS
+    header.
+
+    Parameters
+    ----------
+    nddata : `~astropy.nddata.NDData`
+        An `~astropy.nddata.NDData` object to write to a FITS file.
+
+    filename : str
+        The path of the output FITS file.
+
+    clobber : bool, optional
+        Set to `True` to override ``filename`` if it already exists.
+        The default is `False`.
     """
 
+    hdu = fits.PrimaryHDU()
     if nddata.meta is not None:
-        hdu = fits.PrimaryHDU(header=fits.Header(nddata.meta))
-    else:
-        hdu = fits.PrimaryHDU()
+        for k, v in nddata.meta.iteritems():
+            hdu.header[k] = v
     hdus = [hdu]
 
     hdus.append(fits.ImageHDU(data=nddata.data))
@@ -58,11 +82,11 @@ def basic_nddata_to_fits(nddata, filename, clobber=False):
 
     if nddata.uncertainty is not None:
         hdus.append(fits.ImageHDU(data=nddata.uncertainty.value))
-        hdus[-1].header['EXTNAME'] = 'ERR'
+        hdus[-1].header['EXTNAME'] = 'ERROR'
 
     if nddata.mask is not None:
         hdus.append(fits.ImageHDU(data=nddata.mask.astype(np.int)))
-        hdus[-1].header['EXTNAME'] = 'DQ'
+        hdus[-1].header['EXTNAME'] = 'MASK'
 
     hdulist = fits.HDUList(hdus)
     hdulist.writeto(filename, clobber=clobber)
