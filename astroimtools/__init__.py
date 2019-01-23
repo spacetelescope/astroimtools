bd57eb288ecfc529783cdcdea47aa4620bbc42e3
Larry Bradley <larry.bradley@gmail.com>
2019-01-23 14:28:37 -0500

    Add minimum required python version

--- astroimtools/__init__.py
+++ astroimtools/__init__.py
@@ -6,6 +6,24 @@
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+
+__minimum_python_version__ = '3.5'
+__minimum_numpy_version__ = '1.11'
+
+
+class UnsupportedPythonError(Exception):
+    pass
+
+
+if (sys.version_info <
+        tuple((int(val) for val in __minimum_python_version__.split('.')))):
+    raise UnsupportedPythonError('Photutils does not support Python < {}'
+                                 .format(__minimum_python_version__))
+
+
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
     from .arithmetic import *
