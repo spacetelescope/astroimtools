756db4fad6468fa23101c7346b262d66570c88cb
Larry Bradley <larry.bradley@gmail.com>
2020-04-29 17:14:18 -0400

    Update package infrastructure

--- astroimtools/__init__.py
+++ astroimtools/__init__.py
@@ -1,35 +1,15 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Affiliated packages may add whatever they like to this file, but
+# Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *  # noqa
+from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
-# Enforce Python version check during package import.
-# This is the same check as the one at the top of setup.py
-import sys
-
-__minimum_python_version__ = '3.5'
-__minimum_numpy_version__ = '1.13.0'
-
-
-class UnsupportedPythonError(Exception):
-    pass
-
-
-if (sys.version_info <
-        tuple((int(val) for val in __minimum_python_version__.split('.')))):
-    raise UnsupportedPythonError('Photutils does not support Python < {}'
-                                 .format(__minimum_python_version__))
-
-
-if not _ASTROPY_SETUP_:  # noqa
-    # For egg_info test builds to pass, put package imports here.
-    from .arithmetic import *  # noqa
-    from .cutout_tools import *  # noqa
-    from .filtering import *  # noqa
-    from .nddata_adapters import *  # noqa
-    from .scripts import imarith, imstats  # noqa
-    from .stats import *  # noqa
-    from .utils import *  # noqa
+from .arithmetic import *  # noqa
+from .cutout_tools import *  # noqa
+from .filtering import *  # noqa
+from .nddata_adapters import *  # noqa
+from .scripts import imarith, imstats  # noqa
+from .stats import *  # noqa
+from .utils import *  # noqa
--- licenses/README.rst
+++
@@ -1,8 +0,0 @@
-Licenses
-========
-
-This directory holds license and credit information for works the astroimtools
-package is derived from or distributes, and/or datasets.
-
-The license file for the astroimtools package itself is located in the root
-of this repository.
