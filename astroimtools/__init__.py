1619872eacaf0bbdb458b9a74f47de035a36e554
Larry Bradley <larry.bradley@gmail.com>
2025-02-19 10:50:45 -0500

    Remove deprecated astropy test runner

--- astroimtools/__init__.py
+++ astroimtools/__init__.py
@@ -1,11 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa: F401, F403
-# ----------------------------------------------------------------------------
-
 from .arithmetic import *  # noqa: F401, F403
 from .cutout_tools import *  # noqa: F401, F403
 from .filtering import *  # noqa: F401, F403
@@ -13,3 +7,8 @@ from .nddata_adapters import *  # noqa: F401, F403
 from .scripts import imarith, imstats  # noqa: F401
 from .stats import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
